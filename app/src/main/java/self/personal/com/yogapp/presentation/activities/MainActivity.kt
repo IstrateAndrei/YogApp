0fc0bcc4c702c1795342bb162cc8ff4d6236484f
IstrateAndrei <istrateandrei33@gmail.com>
2018-11-19 17:28:50 +0200

    update on Inner Engineering

--- app/src/main/java/self/personal/com/yogapp/presentation/activities/MainActivity.kt
+++ app/src/main/java/self/personal/com/yogapp/presentation/activities/MainActivity.kt
@@ -1,7 +1,6 @@
-package self.personal.com.yogapp
+package self.personal.com.yogapp.presentation.activities
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
@@ -10,6 +9,8 @@ import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import self.personal.com.yogapp.R
+import self.personal.com.yogapp.presentation.fragments.InnerEngineeringFragment
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -54,7 +55,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // Handle navigation view item clicks here.
         when (item.itemId) {
             R.id.inner_engineering_item -> {
-
+                openInnerEngineering()
             }
             R.id.nav_share -> {
 
@@ -67,4 +68,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
     }
+
+    private fun openInnerEngineering(){
+        val fragment = InnerEngineeringFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).addToBackStack(InnerEngineeringFragment::class.java.simpleName).commit()
+    }
+
 }
